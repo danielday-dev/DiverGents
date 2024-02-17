31579d186db1197f2fcac304ccdaf01dc6c65a86
danielday-dev <141579840+danielday-dev@users.noreply.github.com>
2024-02-17 00:58:39 +0000

    added a spin on death to the pufferfish and adjusted main and menu

--- source/Pufferfish.gd
+++ source/Pufferfish.gd
@@ -1,15 +1,31 @@
 extends CharacterBody2D
 
 @export var moveSpeed : float = 10;
+@export var rotationSpeed : float = 3
 
-var collidedWithDiver : bool = false;
-# Called when the node enters the scene tree for the first time.
+enum FishState{
+	MovingAtPlayer,
+	SpinningAwayFromPlayer,
+}
+var state = FishState.MovingAtPlayer;
+
+
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	velocity.y = -moveSpeed;
+	match state:
+		FishState.MovingAtPlayer:
+			velocity.y = -moveSpeed;
+		FishState.SpinningAwayFromPlayer:
+			$Sprite.rotation += delta * rotationSpeed * PI
 	
 	move_and_slide()
+
+func spinIntoOblivion():
+	var direction = randf_range(-PI, PI) 
+	velocity = Vector2(randf_range(150.0, 250.0), 0.0).rotated(direction)
+	
+	
